f54fbe96503c47906507994f67d9e66edd4191a3
Aqdas <113194530+Aqdas101@users.noreply.github.com>
2023-06-10 21:54:16 -0700

    Update YouTube_Scrapping_Streamlit_App.py

--- YouTube_Scrapping_Streamlit_App.py
+++ YouTube_Scrapping_Streamlit_App.py
@@ -130,7 +130,7 @@ def main():
     st.markdown('<a href="https://www.buymeacoffee.com/aqdasansari">\
     <img src="https://img.buymeacoffee.com/button-api/?\
     text=Buy me a coffee&emoji=&slug=aqdasansari&button_colour=40DCA5&font_colour=ffffff&font_\
-    family=Cookie&outline_colour=000000&coffee_colour=FFDD00" /></a>')
+    family=Cookie&outline_colour=000000&coffee_colour=FFDD00" /></a>', unsafe_allow_html=True)
                 
               
                 
